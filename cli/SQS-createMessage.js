36d42ad3181691c00e6cad9fe89c4123067f0ce2
James Brown <J@mesBrown.com>
2015-04-17 14:45:45 -0400

    Cleaning up code

--- cli/SQS-createMessage.js
+++ cli/SQS-createMessage.js
@@ -9,13 +9,17 @@ AWS.config.loadFromPath('../config.json');
 // Create an sqs client
 var sqs = new AWS.SQS();
 
+var message = {
+	uri			: 'https://farm9.staticflickr.com/8482/8219998460_639a2c2ba9_o.jpg'
+	,width		: 400
+	,height		: 320
+};
+
+var queue = 'https://sqs.us-east-1.amazonaws.com/435206425092/resizeImage'
+
 var params = {
-	MessageBody : JSON.stringify({
-		uri			: 'https://farm9.staticflickr.com/8482/8219998460_639a2c2ba9_o.jpg'
-		,width		: 400
-		,height		: 320
-	})
-	,'QueueUrl'		: 'https://sqs.us-east-1.amazonaws.com/435206425092/resizeImage'
+	MessageBody		: JSON.stringify(message)
+	,'QueueUrl'		: queue
 };
 
 sqs.sendMessage(params, function(err, data){
